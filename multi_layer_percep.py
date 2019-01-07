96f937b0e1691dd26f2cd9025659f1ada731bfba
Sykarius <40524350+Sykarius@users.noreply.github.com>
2019-01-08 00:19:43 +0530

    Update multilayer perceptron
    Adding relu, training bug fixes, one hot encoder was added,

--- multi_layer_percep.py
+++ multi_layer_percep.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 
 
 class Perceptron():
+	
+	"""
+	Functions to call after initializing the object are:
+	
+	set_params -> to set parameters of the network
+	set_activation -> to choose the activation function
+	set_cost_function -> to choose how the cost function is calculated
+	train -> to train the network
+	predict -> after the training to get the predicted value
+
+	If the label is not encoded then you can use the one_hot_encoder
+	for one hot encoding
+	"""
 
 	def __init__(self,x,y):
 		xo = np.ones(shape = (x.shape[0],1))
@@ -57,6 +70,15 @@ class Perceptron():
 	def grad_tanh(self,z):
 		return 1-np.tanh(z)**2
 
+
+	def relu(self,z):
+		return np.maximum(0,z)
+
+
+	def grad_relu(self,z):
+		return z>0
+
+
 	
 	def set_activation(self,act):
 		"""
@@ -75,21 +97,31 @@ class Perceptron():
 		"""
 		if self.act == 0:
 			self.sigmoid(z)
+		
 		elif self.act == 1:
 			self.tanh(z)
+		
+		elif self.act == 2:
+			self.relu(z)
+		
 		else:
-			ValueError
+			raise ValueError
 
 
 	
 	def activate_grad(self,z):
 		"""
-		Gradient of the chosen activation
+		Gradient of the chosen activation function
 		"""
 		if self.act == 0:
 			self.grad_sigmoid(z)
+		
 		elif self.act == 1:
 			self.grad_tanh(z)
+		
+		elif self.act ==2:
+			self.grad_relu(z)
+		
 		else:
 			raise ValueError
 
@@ -98,6 +130,7 @@ class Perceptron():
 	def feedforward(self, inp = None):
 		"""
 		Feed forward through the network
+		Return the output and also the input of each layer
 		"""
 		if inp == None:
 			a = self.bx
@@ -107,15 +140,16 @@ class Perceptron():
 		zlist = []
 		
 		for i in range(self.n_hlayers):
-			z = np.dot(a,np.transpose(self.theta[i]))
+			z = np.dot(a,np.transpose(self.theta[i]))   #Getting input from previous layer and summation
 			zlist.append(z)
-			a = self.activate(z)
-			ao = np.ones(shape = (a.shape[0],1))
-			a = np.hstack((ao,a))
+			a = self.activate(z)                        #passing the summation value through the activation function
+			ao = np.ones(shape = (a.shape[0],1))		
+			a = np.hstack((ao,a))						#adding activation value of bias unit(1)
 
 		z = np.dot(a,np.transpose(self.theta[-1]))
 		return z,zlist
 
+	
 	
 	def sigmoid_cross_entropy(self):
 		z,_ = self.feedforward()
@@ -123,41 +157,71 @@ class Perceptron():
 		self.cost = (-1/self.batch_size)*np.sum(self.by*np.log(a) + (1-self.by)*np.log(1 - a))
 
 	
+	
 	def sigmoid_squared_error(self):
 		z,_ = self.feedforward()
 		a = self.sigmoid(z)
-		self.cost = (1/(2*self.batch_size))*(self.by-a)**2
+		self.cost = (1/(2*self.batch_size))*np.sum(self.by-a)**2
 
 	
+	
 	def softmax_cross_entropy(self):
-		pass
+		z,_ = self.feedforward()
+		a = np.exp(z)
+		a = a/np.sum(a,axis = 1)
+		self.cost = (-1/self.batch_size)*np.sum(self.by*np.log(a) + (1-self.by)*np.log(1 - a))
 
 
+	
 	def set_cost_function(self,cf):
 		"""
 		Setting which cost function to use
 		0-> sigmoid cross entropy
-		1-> sigmoid cross
+		1-> sigmoid squared error
+		2-> softmax cross entropy
 		"""
 		self.cf = cf
 
 
+	
 	def cost_function(self):
+		"""
+		Running the chosen cost function
+		"""
+		
 		if self.cf == 0:
 			self.sigmoid_cross_entropy()
+		
 		elif self.cf == 1:
 			self.sigmoid_squared_error()
+		
+		elif self.cf == 2:
+			self.softmax_cross_entropy()
+		
 		else:
 			raise ValueError
 
 	
+	
 	def cost_grad(self,z):
+		"""
+		Chosing how to calculate error at output layer depending
+		cost function used
+		"""
+		
 		if self.cf == 0:
 			a = self.sigmoid(z)
 			return a - self.by
+		
 		elif self.cf ==1:
 			a = self.sigmoid(z)
 			return  (a-self.by) * a * (1-a)
+		
+		elif self.cf ==2:
+			a = np.exp(z)
+			a = a/np.sum(a,axis = 1)
+			return a-self.by
+		
 		else:
 			raise ValueError
 
@@ -170,9 +234,9 @@ class Perceptron():
 		d = []
 		z,zlist = self.feedforward()
 		a = self.activate(z)
-		d.append(self.cost_grad(z))
+		d.append(self.cost_grad(z))    #error at output(depends on the cost_function used)
 		
-		self.theta_grad = [np.zeros(shape = t.shape) for t in self.theta]
+		self.theta_grad = [np.zeros(shape = t.shape) for t in self.theta]  #initializing the gradients
 		
 		for i in range(n_hlayers):
 			a_grad = self.activate_grad(zlist[::-1][i])
@@ -180,12 +244,12 @@ class Perceptron():
 
 		d.reverse()
 
-		self.theta_grad[0] = self.theta_grad[0] + np.dot(np.transpose(d[0]),self.bx)
+		self.theta_grad[0] = self.theta_grad[0] + np.dot(np.transpose(d[0]),self.bx)	#gradient calculation of input weights(inp-next layer weights)
 		for i in range(1,len(self.theta_grad)):
 			a = self.activate(zlist[i-1])
 			ao = np.ones(shape = (a.shape[0],1))
 			a = np.hstack(ao,a)
-			self.theta_grad[i] = self.theta_grad[i] + np.dot(np.transpose(d[i]),a)
+			self.theta_grad[i] = self.theta_grad[i] + np.dot(np.transpose(d[i]),a)  #gradient calculation of all the other weights
 
 		self.theta_grad = [(1/self.batch_size)*tg for tg in self.theta_grad]
 
@@ -194,49 +258,64 @@ class Perceptron():
 
 
 
-	def train(self, alpha = 0.01, batch_size = 100,stop  = 0.000001):
+	def train(self, alpha = 0.01, batch_size = 100,epochs = 10):
 		"""
 		Traines the network
 		Split to batches
 		Mini-Batch gradient descent
+		alpha is the learning rate
+		epochs is the number of iterations of the entire dataset
 		"""
 		self.batch_size = batch_size
 		self.init_weights()
+		
+		j = 1
+		i=0
 
-		while True:
-			self.bx = self.x[i:i+self.batch_size,:]
-			self.by = self.y[i:i+self.batch_size,:]
+		while j<=epochs:
+			while i+self.batch_size<self.m:
+				self.bx = self.x[i:i+self.batch_size,:]  #Splitting up batches
+				self.by = self.y[i:i+self.batch_size,:]
 
-			print("Cost at current state: ",self.cost_function())
+				print("Cost at current state: ",self.cost_function())
 
-			self.backprop()
+				self.backprop()
 
-			for i in range(len(self.theta)):
-				self.theta[i] = self.theta[i] - (alpha*self.theta_grad[i])   #updating weights
+				for i in range(len(self.theta)):
+					self.theta[i] = self.theta[i] - (alpha*self.theta_grad[i])   #updating weights
 
-			i+=self.batch_size
-			if i+self.batch_size>self.m:
-				break
+				i+=self.batch_size
+			j+=1
 
-			for i in self.theta_grad:
-				cond = (np.abs(i)>stop)
-				if cond.any():
-					break
-			else:
-				print("Gradient is small so the process is stopped")
-				break
+
 
 		print("Trained!!")
 
 
+	
 	def predict(self,inp):
 		"""
 		Run this function to get predictions
+		Return the index of the class predicted
 		"""
 		z,_ = self.feedforward(inp)
-		i = np.argmax(z)
+		i = np.argmax(z)			#finiding index of max value
 
 		return i
+
+
+	def one_hot_encoder(self):
+		"""
+		One hot encoding of labels
+		Given y value is from 1 to n (no of classes)
+		"""
+		
+		ny = np.zeros(shape = (self.m,self.n_classes))
+		
+		for i in range(m):
+			ny[i,self.y[i]-1] = 1    #The n-1 th index will be 1 for each example [where n is the class]
+
+		self.y = ny
 
 
 
